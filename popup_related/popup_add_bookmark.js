ec499378df6c254005906e2214fb9859936832df
abyss_tutor <yejun6210@gmail.com>
2026-01-11 01:47:40 +0900

    Input Field 입력 받는거 js에서 받게 함

--- popup_related/popup_add_bookmark.js
+++ popup_related/popup_add_bookmark.js
@@ -4,4 +4,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
     goBackButton.addEventListener("click", ()=>{
         window.location.href = "/popup_related/popup_default.html"
     })
+
+    submitButton = document.getElementById("SubmitButton");
+    submitButton.addEventListener("click", ()=>{
+        submitForm();
+    })
 })
+
+function submitForm() {
+    keyInputField = document.getElementById("KeyInputField");
+    keyInput = keyInputField.querySelector("input");
+    keyValue = keyInput.value;
+
+    urlInputField = document.getElementById("URLInputField");
+    urlInput = urlInputField.querySelector("input");
+    urlValue = urlInput.value;
+
+    console.log(`Input Key : ${keyValue}, Input URL : ${urlValue}`);
+}
--- popup_related/popup_default.js
+++ popup_related/popup_default.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", () =>{
-    console.log("loaded!");
     addBookmarkButton = document.getElementById("AddBoomarkButton")
 
     addBookmarkButton.addEventListener("click", () =>{
         window.location.href = "/popup_related/popup_add_bookmark.html"
     })
+
+    searchButton = document.getElementById("SearchButton")
+    searchButton.addEventListener("click", ()=>{
+        searchKey()
+    })
 })
+
+function searchKey() {
+    searchBox = document.getElementById("SearchBox")
+
+    inputField = searchBox.querySelector("input")
+    inputKey = inputField.value
+    console.log(`Input Key : ${inputKey}`)
+}
