ec17c0596a94a6ce1260c975a32901bffd3a7d4a
abyss_tutor <yejun6210@gmail.com>
2026-01-12 02:55:22 +0900

    Bookmark 추가 예외 처리 까지 해서 구현 완료

--- popup_related/popup_add_bookmark.js
+++ popup_related/popup_add_bookmark.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     goBackButton = document.getElementById("GoBackButton")
-
     goBackButton.addEventListener("click", ()=>{
         window.location.href = "/popup_related/popup_default.html"
     })
@@ -15,17 +14,55 @@ async function submitForm() {
     keyInputField = document.getElementById("KeyInputField");
     keyInput = keyInputField.querySelector("input");
     keyValue = keyInput.value;
-    keyInput.value = "";
-
+    
     urlInputField = document.getElementById("URLInputField");
     urlInput = urlInputField.querySelector("input");
     urlValue = urlInput.value;
-    urlInput.value = "";
+    
+    result = await chrome.runtime.sendMessage({ event_key : "ADD_BOOKMARK" , name : keyValue, url : urlValue})
+    
+    if (result.isSuccess) {
+        keyInput.value = "";
+        urlInput.value = "";
 
-    isSuccess = await chrome.runtime.sendMessage({ event_key : "ADD_BOOKMARK" , name : keyValue, url : urlValue})
-
-    if (isSuccess) displaySuccessAlram()
-    else displayFailAlram()
+        displayAlaram("Success", result.subText)
+    }
+    else displayAlaram("Fail", result.subText)
 }
 
 
+let alarmTimer = null;
+const ALARM_DUATION = 5000;
+function displayAlaram(type, subTextContent){
+    alarmBox = document.getElementById("AlarmBox")
+    mainText = alarmBox.querySelector("#MainText")
+    subText = alarmBox.querySelector("#SubText")
+
+    alarmBox.classList.remove("Success", "Fail")
+
+    switch (type){
+        case "Success":
+            alarmBox.classList.add("Success");
+            mainText.textContent = "Success to Create Bookmark"
+            subText.textContent = subTextContent
+            break;
+        case "Fail":
+            alarmBox.classList.add("Fail");
+            mainText.textContent = "Fail to Create Bookmark"
+            subText.textContent = subTextContent
+            break;
+        default:
+            console.error(`${type} is not valid`);
+            return;
+    }
+
+    if (alarmTimer){
+        clearTimeout(alarmTimer);
+        alarmTimer = null;
+    }
+
+    alarmTimer = setTimeout(() => {
+        alarmBox.classList.remove("Success", "Fail");
+        alarmTimer = null;
+    }, ALARM_DUATION)
+}
--- service_worker.js
+++ service_worker.js
@@ -3,7 +3,18 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
     switch (request.event_key) {
         case "ADD_BOOKMARK":
-            if (!isValidHttpUrl(request.url)) { break; }
+            if (request.name.trim().length === 0){
+                sendResponse({ "isSuccess" : false, "subText" : "Empty Name : Make sure the name input field is not empty and does not contain only whitespace." });
+                return false;
+            }
+            if (request.url.trim().length === 0){
+                sendResponse({ "isSuccess" : false, "subText" : "Empty URL : Make sure the url input field is not empty and does not contain only whitespace." });
+                return false;
+            }
+            if (!isValidHttpUrl(request.url)) { 
+                sendResponse({ "isSuccess" : false, "subText" : "Invalid URL : Make sure the URL starts with http: or https:" });
+                return false;
+            }
 
             hashCode = crypto.randomUUID()
             chrome.storage.sync.set({
@@ -12,12 +23,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                     url: request.url
                 }
             }).then(() => {
-                sendResponse(true);
+                sendResponse({ "isSuccess" : true, "subText" : "" });
             });
             return true;
     }
 
-    sendResponse(false);
+    sendResponse({ "isSuccess" : false, "subText" : "Unknown Error" });
     return false;
 });
 
